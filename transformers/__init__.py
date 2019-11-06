6f68f8a89ffbd68d6a19a10a84d505309c465b63
Lysandre <lysandre.debut@reseau.eseo.fr>
2019-11-06 20:47:42 +0000

    ALBERT for SQuAD

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -98,6 +98,7 @@ if is_torch_available():
                                 DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP)
 
     from .modeling_albert import (AlbertModel, AlbertForMaskedLM, AlbertForSequenceClassification,
+                                AlbertForQuestionAnswering,
                                 load_tf_weights_in_albert, ALBERT_PRETRAINED_MODEL_ARCHIVE_MAP)
 
     # Optimization
